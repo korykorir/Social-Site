e7522a98909275bb7c1c692da12484ac09102e1d
Kory <stephenkorykorir@gmail.com>
2020-07-05 12:13:02 +0300

    Preapared for deployment

--- server.js
+++ server.js
@@ -17,4 +17,16 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/post'));
 
+
+//serve static assets in production
+
+
+if(process.env.NODE_ENV ==="production"){
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+    })
+}
+
 app.listen(port, ()=>console.log(`sever started in port ${port}`));
